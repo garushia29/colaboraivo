cdab33b5a2bba05e8ce757524ba04decb54409f2
garushia29 <garushia29@gmail.com>
2021-11-04 08:06:11 -0400

    version 2

---
+++ frontend/src/types/types.js
@@ -0,0 +1,14 @@
+
+export const types = {
+    getUsuario: '[Chat] Usuario Cargados',
+    activarChat: '[Chat] Activar Chat',
+    nuevoMensaje: '[Chat] Nuevo Mensaje',
+    cargarMensaje: '[Chat] Cargar Mensaje',
+    limpiarChat: '[Chat] Limpiar Chat',
+
+    getSala: '[Sala] Salas Cargados',
+    limpiarSala: '[Sala] Limpiar Cargados',
+
+    getTable: '[Table] Tables Cargados',
+    limpiarTable: '[Table] Limpiar Cargados',
+};
